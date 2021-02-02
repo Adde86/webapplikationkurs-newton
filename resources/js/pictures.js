f5e0c9c605502c041e0c41a9c1a7307a232103b4
Adde86 <57154481+Adde86@users.noreply.github.com>
2021-02-02 16:38:51 +0100

    Mobile phone image view added

--- resources/js/pictures.js
+++ resources/js/pictures.js
@@ -1,6 +1,7 @@
 const exteriorButton = document.getElementById("btn-exterior");
 const interiorButton = document.getElementById("btn-interior");
 const main = document.getElementById("main");
+let imageList = 'interior';
 
 
 let interior = ['./resources/images/altan1.jpg', './resources/images/altan2.jpg', './resources/images/altan3.jpg', './resources/images/shower1.jpg',
@@ -12,10 +13,12 @@ loadPictures(interior);
 
 exteriorButton.onclick = function(){
     loadPictures(exterior)
+    imageList = 'exterior'
 }
 
 interiorButton.onclick = function(){
     loadPictures(interior);
+    imageList = 'interior'
 }
 
 
@@ -36,9 +39,120 @@ function loadPictures(images){
         main.append(div)
 
         div.addEventListener('click', function(){
-            this.classList.toggle("image-full-screen")
+
+            imageDivs = document.querySelectorAll(".image-wrapper")
+
+            if(this.classList.contains("image-full-screen")){
+                this.classList.remove("image-full-screen")
+                main.classList.remove("single-image")
+
+                for(let imageDiv of imageDivs){
+                        imageDiv.classList.remove("invisible")
+            
+                }
+
+            }else {
+                this.classList.add("image-full-screen")
+                main.classList.add("single-image")
+
+            for(let imageDiv of imageDivs){
+                if(!imageDiv.classList.contains("image-full-screen")){
+                    imageDiv.classList.add("invisible")
+                }
+            }
+            }
+            
         })
     }
 
 }
 
+
+function smallScreenImages(mobileSize) {
+
+    
+        switch(imageList){
+            case 'interior':
+                mobilSize.matches ? mobileImageView(interior) : loadPictures(interior)
+               break;
+            case 'exterior':
+                mobilSize.matches ? mobileImageView(exterior) : loadPictures(exterior)
+                break;
+        }
+   
+    
+    
+    
+
+}
+
+function mobileImageView(images){
+
+    let imageDivs = document.querySelectorAll(".image-wrapper");
+
+    for(let imageDiv of imageDivs) {
+        main.removeChild(imageDiv)
+    }
+
+    let img = document.createElement("img")
+    let div = document.createElement("div")
+    let imageNavDiv = document.createElement("div")
+    let leftButton = document.createElement("button")
+    let rightButton = document.createElement("button")
+    let imageNr = document.createElement("span")
+    let totalNr = document.createElement("span")
+    let nrText = document.createElement("p")
+    imageNr.setAttribute("id", "image-nr")
+    totalNr.setAttribute("id", "total-nr")
+    nrText.append(imageNr)
+    nrText.append(document.createTextNode(" / "))
+    nrText.append(totalNr)
+
+    rightButton.setAttribute("id", "right-btn")
+    leftButton.setAttribute("id", "left-btn")
+    div.classList.add("image-wrapper")
+    div.classList.add("image-fullscreen")
+    main.classList.add("single-image")
+    img.setAttribute("src", images[0])
+    let index = 0;
+
+   
+    imageNr.innerText = `${index +1}`;
+    totalNr.innerText = images.length
+
+    rightButton.innerText = "Next"
+    leftButton.append(document.createTextNode("Prev"))
+
+    imageNavDiv.append(leftButton)
+    imageNavDiv.append(nrText)
+    imageNavDiv.append(rightButton)
+    imageNavDiv.setAttribute("id", "image-nav-div")
+    
+    
+    div.append(img)
+    div.append(imageNavDiv)
+    main.append(div)
+
+    leftButton.addEventListener('click', () => {
+        if(index > 0){
+            index--;
+            img.setAttribute("src", images[index])
+            imageNr.innerText = `${index +1}`;
+            
+        }
+    })
+
+    rightButton.addEventListener('click', () => {
+        if(index < images.length -1){
+            index++;
+            img.setAttribute("src", images[index])
+            imageNr.innerText = `${index +1}`;
+        }
+
+    })
+
+}
+
+var mobilSize = window.matchMedia("(max-width: 375px)")
+smallScreenImages(mobilSize)
+mobilSize.addListener(smallScreenImages)
